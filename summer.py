a6afa109cd38eb6ba1e96caf17127f7025435a6a
Mammad <sobhangheydi@gmail.com>
2026-07-18 19:30:32 +0330

    imported necessary libraries and wrote needed functions to be filled later. (innit commit)

---
+++ summer.py
@@ -0,0 +1,17 @@
+""" this scripts calls APIs and gets 5 yaer history for the two tickers given to it
+and it displays its charts"""
+
+import yfinance as yf
+import pandas as pd
+import plotly.express as px
+
+
+def get_df():
+
+def get_tickers():
+
+def make_avg():
+
+def make_fig():
+
+def show_fig():
