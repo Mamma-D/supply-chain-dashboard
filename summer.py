faece8cf223c10d340d5f32732286f588cb1dc64
Mammad <sobhangheydi@gmail.com>
2026-07-19 15:01:26 +0330

    modified get_ticker function and made its base skeleton.

--- summer.py
+++ summer.py
@@ -20,7 +20,24 @@ def get_df(user_ticker_1, user_ticker_2):
     return (df_1, df_2)
 
 
-def get_tickers():
+def get_ticker():
+    """this function gets a ticker from user and checks whether its valid or not"""
+    while True:
+
+        user_input = input('\n-----Enter the first ticker name-----\n')
+        
+        try:
+            input_cap = user_input.capitalize()
+            
+            #checking to see if the ticker is valid
+            if len(yf.Ticker(input_cap).info) < 2:
+                raise NameError(f"Name {input_cap} does not exist.")
+            
+            break
+
+        except AttributeError, NameError:
+            print("\n-----Your input isn't valid, try again-----")
+
 
 def make_avg():
 
