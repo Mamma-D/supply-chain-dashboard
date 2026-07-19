8cbd85ea069a69a9ee05ba24ab6b513ed174fbb4
Mammad <sobhangheydi@gmail.com>
2026-07-19 14:40:21 +0330

    wrote the inital version of get_df function, now it makes Ticker object and receives their history

--- summer.py
+++ summer.py
@@ -6,7 +6,19 @@ import pandas as pd
 import plotly.express as px
 
 
-def get_df():
+def get_df(user_ticker_1, user_ticker_2):
+    """this function receives each tickers data frame from yahoo finance"""
+
+    ticker_1 = yf.Ticker(str(user_ticker_1))
+    ticker_2 = yf.Ticker(str(user_ticker_2))
+
+    df_1 = ticker_1.history('5y')
+    df_2 = ticker_2.history('5y')
+
+    if df_2.empty or df_1.empty: return
+
+    return (df_1, df_2)
+
 
 def get_tickers():
 
