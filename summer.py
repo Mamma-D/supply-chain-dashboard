7d67366fa0f636c7684056aff455fa3a01c43877
Mammad <sobhangheydi@gmail.com>
2026-07-19 15:07:45 +0330

    reordered get_ticker and get_df functions to use get_ticker inside get_df

--- summer.py
+++ summer.py
@@ -6,6 +6,24 @@ import pandas as pd
 import plotly.express as px
 
 
+def get_ticker():
+    """this function gets a ticker from user and checks whether its valid or not"""
+    while True:
+
+        user_input = input('\n-----Enter the first ticker name-----\n')
+        
+        try:
+            input_cap = user_input.capitalize()
+            
+            #checking to see if the ticker is valid
+            if len(yf.Ticker(input_cap).info) < 2:
+                raise NameError(f"Name {input_cap} does not exist.")
+
+            break
+
+        except AttributeError, NameError:
+            print("\n-----Your input isn't valid, try again-----")
+
 def get_df(user_ticker_1, user_ticker_2):
     """this function receives each tickers data frame from yahoo finance"""
 
@@ -19,24 +37,6 @@ def get_df(user_ticker_1, user_ticker_2):
 
     return (df_1, df_2)
 
-
-def get_ticker():
-    """this function gets a ticker from user and checks whether its valid or not"""
-    while True:
-
-        user_input = input('\n-----Enter the first ticker name-----\n')
-        
-        try:
-            input_cap = user_input.capitalize()
-            
-            #checking to see if the ticker is valid
-            if len(yf.Ticker(input_cap).info) < 2:
-                raise NameError(f"Name {input_cap} does not exist.")
-            
-            break
-
-        except AttributeError, NameError:
-            print("\n-----Your input isn't valid, try again-----")
 
 
 def make_avg():
