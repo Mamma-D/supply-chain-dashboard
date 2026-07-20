4fbf7d0ae854465556f7d9063ae58e4fc7673385
Mammad <sobhangheydi@gmail.com>
2026-07-20 20:37:39 +0330

    imported graph_objects

--- summer.py
+++ summer.py
@@ -4,6 +4,7 @@ and it displays its charts"""
 import yfinance as yf
 import plotly.express as px
 from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 def get_ticker(n_of_runs=2):
