1f768ddcf6531fe8ac87a1c075965cce7fe5075c
Mammad <sobhangheydi@gmail.com>
2026-07-23 14:26:30 +0330

    Made the percentage change slider

--- dashboard.py
+++ dashboard.py
@@ -72,6 +72,7 @@ with col_1:
 
         # Making a number input to get moving average period
         ma_n = st.number_input('Enter the moving average day period', value=30)
+        user_pct = st.slider('Choose the percentage change in the material', value=0)
 
         # Making a slider to get moving average period
         # ma_n = st.slider('Choose the moving average day period', min_value=1, max_value=365, value=30)
