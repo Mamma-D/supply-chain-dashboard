8f0fa3773d9ee04fc76f9e3bfb61a1ef349d7e3b
Mammad <sobhangheydi@gmail.com>
2026-07-23 21:19:50 +0330

    Changed the slider name in col 1. dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -79,7 +79,7 @@ with col_1:
         # Making a slider to get moving average period
         # ma_n = st.slider('Choose the moving average day period', min_value=1, max_value=365, value=30)
 
-        user_pct = st.slider('Choose the percentage change in the material', value=0, min_value=-100, max_value=100)
+        user_pct = st.slider('Simulate Material Price Shock (%)', value=0, min_value=-100, max_value=100)
         summer.change_affect_dynamic(company_df, material_df, user_pct)
 
         # if correlation_calculated: 
