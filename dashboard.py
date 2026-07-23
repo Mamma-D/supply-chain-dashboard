42c2f2497386cf311453e2f9dcfdd477a8a56ee5
Mammad <sobhangheydi@gmail.com>
2026-07-23 14:34:39 +0330

    Passed the user_pct into functions with the bool convesion. summer.py

--- dashboard.py
+++ dashboard.py
@@ -77,7 +77,7 @@ with col_1:
         ma_n = st.number_input('Enter the moving average day period', value=30)
         user_pct = st.slider('Choose the percentage change in the material', value=0)
 
-        summer.change_affect(company_df, material_df, user_pct)
+        summer.change_affect(company_df, material_df, bool(user_pct))
         # Making a slider to get moving average period
         # ma_n = st.slider('Choose the moving average day period', min_value=1, max_value=365, value=30)
 
@@ -85,7 +85,7 @@ with col_1:
         data_frames_with_average = summer.make_avg(dataframes_received, ma_n)
 
         # Creating the plot in plotly
-        figs_created = summer.make_fig(data_frames_with_average)
+        figs_created = summer.make_fig(data_frames_with_average, bool(user_pct))
 
 
 #* -----Running column two-----
