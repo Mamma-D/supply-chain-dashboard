2683d48d86de7ecf035ca5f3cc809654db04cc80
Mammad <sobhangheydi@gmail.com>
2026-07-23 15:14:50 +0330

    Shorten the pct_change slider values to 300% and showcased the correlation. dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -78,8 +78,11 @@ with col_1:
         # Making a slider to get moving average period
         # ma_n = st.slider('Choose the moving average day period', min_value=1, max_value=365, value=30)
 
-        user_pct = st.slider('Choose the percentage change in the material', value=0, min_value=-500, max_value=500)
-        summer.change_affect(company_df, material_df, user_pct)
+        user_pct = st.slider('Choose the percentage change in the material', value=0, min_value=-300, max_value=300)
+        correlation_calculated = summer.change_affect(company_df, material_df, user_pct)
+
+        if correlation_calculated: 
+            st.metric(label="Correlation", value=correlation_calculated.round(3), format='plain')
 
         # Creating the moving average column in the data frame
         data_frames_with_average = summer.make_avg(dataframes_received, ma_n)
