3429d0ecf31c3599e5fb68fb5d65f352af62e698
Mammad <sobhangheydi@gmail.com>
2026-07-23 15:04:28 +0330

    Fixed the if condition checking in change_affect(). summer.py

--- summer.py
+++ summer.py
@@ -228,7 +228,7 @@ def show_fig(fig):
 def change_affect(company_df, material_df, change_prc=0):
     """this function simulates a price change in material and it's effect on the company stock price"""
 
-    if change_prc:
+    if bool(change_prc):
 
         company_df['pct_change'] = company_df['Close'].pct_change()
         material_df['pct_change'] = material_df['Close'].pct_change()
